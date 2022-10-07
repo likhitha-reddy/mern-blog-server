af63059fbc9f135b08851f60c11ea969e47655eb
likhitha <likhitha0341@gmail.com>
2022-10-07 23:27:16 +0530

    done

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
+});
 
 {/*
 app.use(function(req, res, next) {
